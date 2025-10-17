1be9ae23046982702646265462d57f752c573b14
harnwalN <neelaksh.harnwal@gmail.com>
2025-10-17 08:59:23 -0400

    Add Breed Counting template

--- Week_3/S2015_D3_BreedCounting.java
+++ Week_3/S2015_D3_BreedCounting.java
@@ -2,7 +2,7 @@ package Week_3;
 import java.util.*;
 import java.io.*;
 
-// Breed Counting - USACO 2015 December Contest, Bronze Question 3
+// Breed Counting - USACO 2015 December Contest, Silver Question 3
 public class S2015_D3_BreedCounting {
     public static void main(String[] args) throws IOException{
         
