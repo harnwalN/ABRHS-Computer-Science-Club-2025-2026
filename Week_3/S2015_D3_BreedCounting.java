7b9400429ebca525dd72fd934f82fdfacdc660bc
harnwalN <neelaksh.harnwal@gmail.com>
2025-10-19 12:56:26 -0400

    Add space

--- Week_3/S2015_D3_BreedCounting.java
+++ Week_3/S2015_D3_BreedCounting.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 // Breed Counting - USACO 2015 December Contest, Silver Question 3
 public class S2015_D3_BreedCounting {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         
     }
 }
